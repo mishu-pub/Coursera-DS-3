4235b71f2ead68f6cab192d3bb1c00edbdc8842e
Mihai Georgescu <mishu_pub@yahoo.com>
2016-02-26 22:29:48 +0100

    added download fixed local paths

--- CourseProj.R
+++ CourseProj.R
@@ -1,5 +1,14 @@
+library(dplyr)
+
+#download data
+
+#dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+#download.file(dataURL, "data.zip")
+#unzip data
+#unzip("data.zip")
+
 #read features file
-flabels <- read.table("/Users/m.georgescu/weiterbildung/coursera/data\ science/getting-cleaning/week3/UCI\ HAR\ Dataset/features.txt", header=FALSE)
+flabels <- read.table("UCI\ HAR\ Dataset/features.txt", header=FALSE)
 featureNames <- flabels$V2
 
 #identify just interesting features, containing mean or std
@@ -8,9 +17,9 @@ featureNames[interestingFeaturesIdx]
 
 
 #read training set
-train <- read.table("/Users/m.georgescu/weiterbildung/coursera/data\ science/getting-cleaning/week3/UCI\ HAR\ Dataset/train/X_train.txt", header=FALSE)
+train <- read.table("UCI\ HAR\ Dataset/train/X_train.txt", header=FALSE)
 #read testing set
-test <- read.table("/Users/m.georgescu/weiterbildung/coursera/data\ science/getting-cleaning/week3/UCI\ HAR\ Dataset/test/X_test.txt", header=FALSE)
+test <- read.table("UCI\ HAR\ Dataset/test/X_test.txt", header=FALSE)
 #merge them
 allFeatures <- rbind(train, test)
 
@@ -18,14 +27,14 @@ features <- allFeatures %>% select(interestingFeaturesIdx)
 names(features) <- featureNames[interestingFeaturesIdx]
 
 #read label values for train
-ytrain <- read.table("/Users/m.georgescu/weiterbildung/coursera/data\ science/getting-cleaning/week3/UCI\ HAR\ Dataset/train/y_train.txt", header=FALSE)
+ytrain <- read.table("UCI\ HAR\ Dataset/train/y_train.txt", header=FALSE)
 #read label values for test
-ytest <- read.table("/Users/m.georgescu/weiterbildung/coursera/data\ science/getting-cleaning/week3/UCI\ HAR\ Dataset/test/y_test.txt", header=FALSE)
+ytest <- read.table("UCI\ HAR\ Dataset/test/y_test.txt", header=FALSE)
 #merge them
 yall <- rbind(ytrain, ytest)
 
 #read values to activites legend
-activityLabels <- read.table("/Users/m.georgescu/weiterbildung/coursera/data\ science/getting-cleaning/week3/UCI\ HAR\ Dataset/activity_labels.txt", header=FALSE)
+activityLabels <- read.table("UCI\ HAR\ Dataset/activity_labels.txt", header=FALSE)
 #resolve the labels and have them nice now
 mergedLabels <- merge(yall, activityLabels)
 names(mergedLabels) <- c("id", "Activity")
@@ -33,8 +42,8 @@ labels <- mergedLabels %>% select(Activity)
 
 
 #read subject data 
-subjectTrain <- read.table("/Users/m.georgescu/weiterbildung/coursera/data\ science/getting-cleaning/week3/UCI\ HAR\ Dataset/train/subject_train.txt", header=FALSE)
-subjectTest <- read.table("/Users/m.georgescu/weiterbildung/coursera/data\ science/getting-cleaning/week3/UCI\ HAR\ Dataset/test/subject_test.txt", header=FALSE)
+subjectTrain <- read.table("UCI\ HAR\ Dataset/train/subject_train.txt", header=FALSE)
+subjectTest <- read.table("UCI\ HAR\ Dataset/test/subject_test.txt", header=FALSE)
 subject <- rbind(subjectTrain, subjectTest)
 names(subject) <- c("Subject")
 
@@ -46,4 +55,4 @@ coolData <- cbind(subject, features, labels)
 #now to make that tidy data set
 
 tidyNiceData <- coolData %>% group_by(Activity, Subject) %>% summarise_each(funs(mean))
-write.table(tidyNiceData, "tidy.csv")
+write.table(tidyNiceData, "tidy.txt", row.name=FALSE )
